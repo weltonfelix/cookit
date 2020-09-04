3d347e8d16888c862e750f4bf5f4c0047eb743b1
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-09-03 23:54:25 -0300

    web: Initial Folders and Router setup

--- packages/web/src/pages/Landing/index.tsx
+++ packages/web/src/pages/Landing/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import './styles.css'
+import './styles';
 
-const Landing: React.FC = () => {
-  return <div>Hello, Cookit!</div>;
-}
+const Landing: React.FC = () => <div>Hello, Cookit!</div>;
 
 export default Landing;
--- packages/web/src/routes.tsx
+++ packages/web/src/routes.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing'
+import Landing from './pages/Landing';
 
-function Routes() {
-    return (
-        <BrowserRouter>
-            <Route path="/" exact component={Landing} />
-        </BrowserRouter>
-    )
-}
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Route path="/" exact component={Landing} />
+  </BrowserRouter>
+);
 
-export default Routes
+export default Routes;

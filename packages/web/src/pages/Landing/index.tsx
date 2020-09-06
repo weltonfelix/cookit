52648eaf59375bfd89ada02b6791d5b29a7f5d94
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-09-05 22:55:27 -0300

    web: landing page started (logo and headings)

--- packages/web/src/pages/Landing/index.tsx
+++ packages/web/src/pages/Landing/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 import './styles.css';
+import logotype from '../../assets/images/logos/logotype.svg';
 
-const Landing: React.FC = () => <div>Hello, Cookit!</div>;
+const Landing: React.FC = () => {
+  const inibireslint = '';
+
+  return (
+    <div id="page-landing">
+      <header>
+        <img src={logotype} alt="Cookit!" />
+      </header>
+      <main>
+        <div id="headings">
+          <h1>Não sabe o que fazer pro almoço{/* MUDAR */}, né?</h1>
+          <h3>
+            Sem problemas! Escreve aqui embaixo os ingredientes que você tem na
+            cozinha, separados por vírgula:
+          </h3>
+        </div>
+        <div id="search-bar">
+          <input type="text" />
+        </div>
+      </main>
+      <section id="popular-ingredients"> </section>
+    </div>
+  );
+};
 
 export default Landing;

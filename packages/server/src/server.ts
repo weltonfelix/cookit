ed8889b487020e7d7e8b7798910efe992ccd2336
Welton Felix <contato.weltonf@gmail.com>
2020-11-13 14:35:14 -0300

    Create server initial structure

---
+++ packages/server/src/server.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log(`Server started on port ${'3333'}!`);
+});

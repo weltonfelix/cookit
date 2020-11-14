2aba824325c9f923a306c7d3f21cc5f3e0f16b02
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-11-14 16:48:00 -0300

    Update old code

--- packages/web/src/pages/Landing/index.tsx
+++ packages/web/src/pages/Landing/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import PopularIngredient from '../../components/PopularIngredient';
 
 import './styles.css';
+import { MdSearch } from 'react-icons/md';
 import logotype from '../../assets/images/logos/logotype.svg';
 
 const Landing: React.FC = () => {
-  const inibireslint = '';
+  const [currentMeal, setCurrentMeal] = useState('');
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+
+    if (hour >= 4 && hour <= 10) {
+      setCurrentMeal('café da manhã');
+    } else if (hour > 10 && hour <= 16) {
+      setCurrentMeal('almoço');
+    } else {
+      setCurrentMeal('jantar');
+    }
+  }, []);
 
   return (
     <div id="page-landing">
@@ -13,17 +28,23 @@ const Landing: React.FC = () => {
       </header>
       <main>
         <div id="headings">
-          <h1>Não sabe o que fazer pro almoço{/* MUDAR */}, né?</h1>
+          <h1>Não sabe o que fazer pro {currentMeal}, né?</h1>
           <h3>
             Sem problemas! Escreve aqui embaixo os ingredientes que você tem na
             cozinha, separados por vírgula:
           </h3>
         </div>
         <div id="search-bar">
-          <input type="text" />
+          <input type="text" placeholder="Ingredientes" />
+          <MdSearch />
         </div>
       </main>
-      <section id="popular-ingredients"> </section>
+      <section id="popular-ingredients-container">
+        <h3>Ingredientes Populares</h3>
+        <div id="popular-ingredients">
+          <PopularIngredient />
+        </div>
+      </section>
     </div>
   );
 };
